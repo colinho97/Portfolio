ee1a84e95eadeb4714b173df3fe374b4a9722b9e
Colin Ho <colinho97@gmail.com>
2022-06-04 22:48:38 -0400

    added in basic titles for all sections, and added in animation for navLinks when section is in focus using intersection observer, which is added in nav.js

---
+++ js/nav.js
@@ -0,0 +1,27 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav a');
+
+const options = {
+    root: null, //this represent the viewport
+    rootMargin: '0px 0px -100%',
+}
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting === true) {
+            // console.log(entry.target);
+            const id = entry.target.id
+            // console.log(id);
+            const navActive = document.querySelector(`a[href="${id}"]`);
+            // console.log(navActive);
+            navLinks.forEach(link => link.classList.remove('active'));
+            navActive.classList.add('active');
+        } else {
+            return;
+        }
+    });
+},options);
+
+sections.forEach(section => {
+    observer.observe(section);
+})
