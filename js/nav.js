affadc16d468f85070d8e0889ba9f0e5d5390ad0
Colin Ho <colinho97@gmail.com>
2022-07-20 20:17:42 -0400

    added in hamburger nav bar

--- js/nav.js
+++ js/nav.js
@@ -33,3 +33,21 @@ window.onscroll = function() {
         navActive.classList.add('active');
     }
 };
+
+
+//Hamburger Nav Functionality
+
+const hamburger = document.querySelector('.hamburgerContainer');
+const navMenu = document.querySelector('.navBarItemList');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.navLink').forEach((link) => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    })
+})
